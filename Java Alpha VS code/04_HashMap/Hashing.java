492f80f0808b7f5f06d1b22319812bf56d485b3f
Sahiljagdale07 <sahilashokjagdale@gmail.com>
2025-02-06 20:30:29 +0530

    init commit

---
+++ Java Alpha VS code/04_HashMap/Hashing.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Hashing {
+    public static void main(String[]args) {
+      HashMap<String, Integer> map = new HashMap<>();
+
+      map.put("India", 120);
+      map.put("China", 150);
+      map.put("US",80);
+
+      System.out.println(map);
+
+    }
+}
